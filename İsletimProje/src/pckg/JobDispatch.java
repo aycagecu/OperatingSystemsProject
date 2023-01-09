817cd47abf1d3a60695b0e96992112796b369c08
HikmetSaylam <hekmatsailam362@gmail.com>
2023-01-09 03:01:24 +0300

    Son hali

--- İsletimProje/src/pckg/JobDispatch.java
+++ İsletimProje/src/pckg/JobDispatch.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Random;
+
+import pckg.Proses.processState;
+
 import java.util.Iterator;
 
 public class JobDispatch 
@@ -22,6 +26,37 @@ public class JobDispatch
 		
 	}
 	
+	public void processControl(UserJobQueue userjob,int time) {
+		for(int i=0;i<userjob.firstQueue.size();i++) {
+			Proses p=userjob.firstQueue.get(i);
+			if((time-p.askiZamani)>=20) {
+				p.state=processState.zamanAşımı;
+				p.process.destroy();
+				p.ProsesDurum(time);
+				userjob.firstQueue.remove(userjob.firstQueue.indexOf(p));
+			} 
+		}
+		for(int j=0;j<userjob.secondQueue.size();j++) {
+			Proses p=userjob.secondQueue.get(j);
+			if((time-p.askiZamani)>=20) {
+				p.state=processState.zamanAşımı;
+				p.process.destroy();
+				p.ProsesDurum(time);
+				userjob.secondQueue.remove(userjob.secondQueue.indexOf(p));
+			} 
+		}
+		for(int k=0;k<userjob.thirdQueue.size();k++) {
+			Proses p=userjob.thirdQueue.get(k);
+			if( (time-p.askiZamani)>=20) {
+				p.state=processState.zamanAşımı;
+				p.process.destroy();
+				p.ProsesDurum(time);
+				userjob.thirdQueue.remove(userjob.thirdQueue.indexOf(p));
+			}
+		}
+	}
+	
+	
 	public void addProsesToList(Proses Proses)
 	{
 		ProsesList.add(Proses);
@@ -37,20 +72,12 @@ public class JobDispatch
 			{
 				jobDispatch.RealTimeQueue.add(Proses);
 				time+= Proses.getBurstTime();
-				/*fcfs.findWaitingTime(RealTimeQueue);
-				System.out.println(RealTimeQueue.size());
-				System.out.println(RealTimeQueue.peek());
-				RealTimeQueue.remove();*/
-				
-				
 			}
 			
 			 else if (Proses.getArrivalTime()<=time){
 				jobDispatch.UserQueue.add(Proses);
 				time++;
 			}
-		
-			//System.out.println(Proses.getBurstTime());
 		}
 	}
 	
@@ -60,10 +87,9 @@ public class JobDispatch
 		MultiLevelQueue multilevelQueue;
 		multilevelQueue = new MultiLevelQueue();
 		Iterator<Proses> iterator=RealTimeQueue.iterator();
-		System.out.println("\nRealtime Queue");
+
 		int sonuc=0;
 		int time=0;
-		int k=0;
 		//realtimequeue yu geziyor
 		Proses pro=null;
 		if(iterator.hasNext()) pro=iterator.next(); // gercek zamanli kuyruktaki ilk proses aliniyor
@@ -75,36 +101,40 @@ public class JobDispatch
 					//Proseslerin varis  zamani ile time Ä± karÅŸÄ±laÅŸtÄ±rÄ±yor varÄ±ÅŸ zamanÄ± timeden bÃ¼yÃ¼kse dÃ¶ngÃ¼den Ã§kÄ±yor.
 					if(pro.getArrivalTime()>time) // yuksek oncelikli kuyrugun zamani gelmediyse
 					{
-						System.out.println("time:" +time +"");
 						time=multilevelQueue.RunMultiLevelQueue(userjob , time,pro.getArrivalTime());
-						System.out.println(pro.getArrivalTime()+": arrival ,time: "+time);
+				
 						if(pro.getArrivalTime()<=time)
 						{
 							
 							// gecicireal.add(pro);
 							int sayacStart=0;
 							//burada yuksek oncelikli prosesı direkt calistirsin
-							while(pro.getRemainingTime()>= 0) // kalan zaman 0 olana kadar calıstırıp kalan zaman guncellensın
+							while(pro.getRemainingTime()> 0) // kalan zaman 0 olana kadar calıstırıp kalan zaman guncellensın
 							{
 
 								
 								if (sayacStart==0)
 								{
-									pro.ProsesRunning(); //prosess builder
-									pro.ProsesDurum();
+									pro.state=processState.başladı;
+									pro.ProsesDurum(time);
 									sayacStart++;
+									time++;
+									processControl(userjob,time);
 								}
 								else 
 								{
-									pro.ProsesDurum();
+									pro.state=processState.yürütülüyor;
+									pro.ProsesDurum(time);
+									time++;
+									processControl(userjob,time);
 								}
 								pro.updateRemainingTime();
-								
-								
 							}
 							//iterator.remove(); 
-							time+=pro.getBurstTime();
-							System.out.println("time:" +time +" ");
+							
+							pro.state=processState.sonlandı;
+							pro.ProsesDurum(time);
+							pro.process.destroy();
 						
 							if(iterator.hasNext()) pro=iterator.next();
 							else pro=null;
@@ -120,28 +150,36 @@ public class JobDispatch
 						// gecicireal.add(pro);
 						int sayacStart=0;
 						//burada yuksek oncelikli prosesı direkt calistirsin
-						while(pro.getRemainingTime()>= 0) // kalan zaman 0 olana kadar calıstırıp kalan zaman guncellensın
+						while(pro.getRemainingTime()> 0) // kalan zaman 0 olana kadar calıstırıp kalan zaman guncellensın
 						{
 
-							
 							if (sayacStart==0)
 							{
-								pro.ProsesRunning(); //prosess builder
-								pro.ProsesDurum();
+								pro.state=processState.başladı;
+								pro.ProsesDurum(time);
 								sayacStart++;
+								time++;
+								processControl(userjob,time);
 							}
 							else 
 							{
-								pro.ProsesDurum();
+								pro.state=processState.yürütülüyor;
+								pro.ProsesDurum(time);
+								time++;
+								processControl(userjob,time);
 							}
+							
 							pro.updateRemainingTime();
 							
 							
 						}
 						//iterator.remove(); 
-						time+=pro.getBurstTime();
-						System.out.println("time:" +time +" ");
+						
 					
+						pro.state=processState.sonlandı;
+						pro.ProsesDurum(time);
+						pro.process.destroy();
+						
 						if(iterator.hasNext()) pro=iterator.next();
 						else pro=null;
 						
@@ -150,52 +188,15 @@ public class JobDispatch
 			}
 			else {	//realtime boşsa/bitmişse
 				if(!userjob.IsEmpty()) {
-					System.out.println("time:" +time +"");
+
 					time=multilevelQueue.RunMultiLevelQueue(userjob ,time,65000);
-					System.out.println("realtime bitti"+": arrival ,time: "+time);
 				}
 			}
 		
 			
 		}iterator.remove();
-		//fcfs.findavgTime(gecicireal);//fcfs Ã§alÄ±ÅŸÄ±yor
-		//gecicireal.clear();// geÃ§iÃ§i queue temizleniyor.
-		/*System.out.println("\nUserJob Queue");
-		for (Proses Proses : UserQueue) 
-		{	
-			System.out.println(Proses.getPriority());
-			
-			
-		}
 		
-		
-		System.out.println("\nFirst Queue");
-		for (Proses Proses : userjob.firstQueue) 
-		{
-			System.out.println(Proses.getPriority());
-		}
-		
-		System.out.println("\nSecond Queue");
-		for (Proses Proses : userjob.secondQueue) 
-		{
-			System.out.println(Proses.getPriority());
-		}
-		
-		System.out.println("\nThird Queue");
-		for (Proses Proses : userjob.thirdQueue) 
-		{	
-			System.out.println(Proses.getPriority());
-		}*/
 	}
 
-	
-	/*
-	public void writeProses()
-	{
-		for (Proses Proses : ProsesList) 
-		{
-			System.out.println(Proses.getBurstTime());
-		}
-	}
-	*/
+
 	}
